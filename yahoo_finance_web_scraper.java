91b599f27dbaf62f6002398d4c8a94ec34d65711
pierrebourd <43405934+pierrebourd@users.noreply.github.com>
2020-04-21 22:55:01 +0200

    Update yahoo_finance_web_scraper.java

--- yahoo_finance_web_scraper.java
+++ yahoo_finance_web_scraper.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 /*The following program is a simple Web Scraper, which allows the user to make requests about a specific security.
   The program can find the current price of the specified security as well as download the daily data by scraping
-  Yahoo Finance (https://finance.yahoo.com). It uses the ChromeDriver (https://chromedriver.chromium.org/) as well
+  Yahoo Finance (https://finance.yahoo.com). It uses the Chrome Driver (https://chromedriver.chromium.org/) as well
 	as Selenium's libraries for Web Scraping (https://www.selenium.dev/downloads/). */
 
 public class yahoo_finance_web_scraper {
